ae6404cd2b484dc2a1c161543843a0af61b4c460
codeAKstan <boystanismoses@gmail.com>
2023-10-15 07:48:09 -0700

    Modified the function to be able to print unsupported specifiers

--- _printf.c
+++ _printf.c
@@ -1,13 +1,36 @@
 #include <stdlib.h>
 #include "main.h"
+void print_null_or_str(char *s, size_t *count);
 /**
- * _printf - entry point. custom print
- * @format: a character string
+ * print_null_or_str - entry point
+ * @s: a string of characters
+ * @count: characrter count
+ */
+void print_null_or_str(char *s, size_t *count)
+{
+	size_t len = 0;
+
+	if (s == NULL)
+	{
+		write(1, "(null)", 6);
+		(*count) += 6;
+	}
+	else
+	{
+		while (s[len] != '\0')
+			len++;
+		write(1, s, len);
+		(*count) += len;
+	}
+}
+/**
+ * _printf - entry point. custom printf
+ * @format: a charcter string
  * Return: return type is a string
  */
 int _printf(const char *format, ...)
 {
-	size_t c, count = 0, len = 0;
+	size_t c, count = 0;
 	char *s;
 	va_list args;
 
@@ -24,24 +47,19 @@ int _printf(const char *format, ...)
 			if (*format == '\0')
 				break;
 			if (*format == '%')
-			{
-				write(1, format, 1);
-				count++;
-			}
+				write(1, format, 1), count++;
 			else if (*format == 'c')
 			{
 				c = va_arg(args, int);
-				write(1, &c, 1);
-				count++;
+				write(1, &c, 1), count++;
 			}
 			else if (*format == 's')
 			{
 				s = va_arg(args, char *);
-				while (s[len] != '\0')
-					len++;
-				write(1, s, len);
-				count += len;
+				print_null_or_str(s, &count);
 			}
+			else
+				write(1, format - 1, 2), count += 2;
 		}
 		format++;
 	}
