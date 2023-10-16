9cdbfd4c2bc65a88ad28aa465e8daec229163a05
Nomeh Favour <chimamaka220@gmail.com>
2023-10-16 01:39:35 -0700

    Added main.h

--- main.h
+++ main.h
@@ -1,9 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdlib.h>
+#include <stdio.h>
 #include <stdarg.h>
 #include <unistd.h>
 
 int _putchar(char c);
-int _printf(const char *format, ...);
+int _printf(const char format, ...);
+void print_unknown_specifier(char specifier, size_tcount);
+void print_null_or_str(char s, size_tcount);
 #endif
